72092f932fff035d1fa6a3c1a1d32da7e95e6f32
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-07-03 19:44:39 +1000

    Add react-style demo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,23 @@ import StyledComponent from './components/StyledComponent/StyledComponent';
 
 import cssModule from './styles/css-module.css';
 import object from './styles/object.js';
+import reactStyle from './styles/react-style.js';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
+        <h1>React Stylable Experiment</h1>
+
+        <h2>CSS Modules</h2>
         <StyledComponent styles={cssModule} />
+
+        <h2>Plain Object</h2>
         <StyledComponent styles={object} />
+
+        <h2>React Style</h2>
+        <StyledComponent styles={reactStyle} />
       </div>
     );
   }
---
+++ src/components/App/styles/react-style.js
@@ -0,0 +1,24 @@
+import StyleSheet from 'react-style';
+
+export default StyleSheet.create({
+  foo: {
+    color: 'red'
+  },
+  foo_hover: {
+    fontWeight: 'bold'
+  },
+
+  bar: {
+    color: 'green'
+  },
+  bar_hover: {
+    fontWeight: 'bold'
+  },
+
+  baz: {
+    color: 'blue'
+  },
+  baz_hover: {
+    fontWeight: 'bold'
+  }
+});
