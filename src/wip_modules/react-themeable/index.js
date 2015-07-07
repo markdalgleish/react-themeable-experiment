ea58ad4a9b1427c4c478b5c7f4a2267ba4e06dce
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-07-08 07:06:16 +1000

    Fold themeable into react-themeable

--- src/wip_modules/react-themeable/index.js
+++ src/wip_modules/react-themeable/index.js
@@ -1,5 +1,13 @@
-import themeable from 'themeable';
+import assign from 'lodash.assign';
+
+const truthy = x => x;
 
 export default theme => (key, ...names) => {
-  return { key, ...themeable(theme)(...names) };
+  const styles = names
+    .map(name => theme[name])
+    .filter(truthy);
+
+  return typeof styles[0] === 'string' ?
+    { key, className: styles.join(' ') } :
+    { key, style: assign({}, ...styles) };
 };
