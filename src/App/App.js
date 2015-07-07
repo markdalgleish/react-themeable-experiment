9a16e5d2579c9b8612796f1d05b9ef09458d11a5
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-07-07 17:33:07 +1000

    Reorganise code

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Radium from 'radium';
 
-import ThemedComponent from './components/ThemedComponent/ThemedComponent';
+import ThemedComponent from './ThemedComponent';
 const RadiumThemedComponent = Radium(ThemedComponent);
 
-import cssModule from './styles/css-module.css';
-import object from './styles/object.js';
-import reactStyle from './styles/react-style.js';
-import radiumTheme from './styles/radium.js';
+import cssModulesTheme from './themes/css-modules.css';
+import objectTheme from './themes/object.js';
+import reactStyleTheme from './themes/react-style.js';
+import radiumTheme from './themes/radium.js';
 
 export default class App extends Component {
 
@@ -17,13 +17,13 @@ export default class App extends Component {
         <h1>React Themeable Experiment</h1>
 
         <h2>CSS Modules</h2>
-        <ThemedComponent theme={cssModule} />
+        <ThemedComponent theme={cssModulesTheme} />
 
         <h2>Plain Object</h2>
-        <ThemedComponent theme={object} />
+        <ThemedComponent theme={objectTheme} />
 
         <h2>React Style</h2>
-        <ThemedComponent theme={reactStyle} />
+        <ThemedComponent theme={reactStyleTheme} />
 
         <h2>Radium</h2>
         <RadiumThemedComponent theme={radiumTheme} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import App from './components/App/App';
+import App from './App/App';
 
 if (typeof document !== 'undefined') {
   React.render(<App />, document.getElementById('outlet'));
